c5f8cc239824dfb71a9443c8cd6bc7a86603a4c3
Cristian Butzke <cristian.btzk@gmail.com>
2022-07-04 11:20:00 -0300

    Fix autoload path

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($class) {
-  if (file_exists("classes".DIRECTORY_SEPARATOR.$class.".class.php"))
-    require_once("classes".DIRECTORY_SEPARATOR.$class.".class.php");
+  if (file_exists(__DIR__."/classes".DIRECTORY_SEPARATOR.$class.".class.php"))
+    require_once(__DIR__."/classes".DIRECTORY_SEPARATOR.$class.".class.php");
 });
 ?>
