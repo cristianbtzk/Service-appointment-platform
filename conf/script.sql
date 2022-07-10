f30a4ee4b49fcad501ed5cd28f1398e5e148bfe1
Cristian Butzke <cristian.btzk@gmail.com>
2022-07-10 13:42:38 -0300

    Update sql script

--- conf/script.sql
+++ conf/script.sql
@@ -33,32 +33,6 @@ CREATE TABLE IF NOT EXISTS `sapp`.`categories` (
   `description` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `sapp`.`services` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '	',
-  `user_id` INT NOT NULL,
-  `category_id` INT NOT NULL,
-  `status_id` INT NOT NULL,
-  `title` VARCHAR(45) NULL,
-  `description` TEXT NULL,
-  `min_date` DATETIME NULL,
-  `max_date` DATETIME NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_services_status1`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `sapp`.`service_status` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_services_categories1`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `sapp`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_services_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `sapp`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
 CREATE TABLE IF NOT EXISTS `sapp`.`states` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
@@ -73,6 +47,58 @@ CREATE TABLE IF NOT EXISTS `sapp`.`cities` (
   CONSTRAINT `fk_city_state1`
     FOREIGN KEY (`state_id`)
     REFERENCES `sapp`.`states` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE IF NOT EXISTS `sapp`.`addresses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `city_id` INT NOT NULL,
+  `house_number` INT NULL,
+  `street` VARCHAR(45) NULL,
+  `postal_code` VARCHAR(45) NULL,
+  `district` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_addresses_cities1`
+    FOREIGN KEY (`city_id`)
+    REFERENCES `sapp`.`cities` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_addresses_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `sapp`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE IF NOT EXISTS `sapp`.`services` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '	',
+  `address_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  `status_id` INT NOT NULL,
+  `title` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  `min_date` DATETIME NULL,
+  `max_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_services_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `sapp`.`addresses` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_services_status1`
+    FOREIGN KEY (`status_id`)
+    REFERENCES `sapp`.`service_status` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_services_categories1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `sapp`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_services_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `sapp`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
@@ -97,26 +123,6 @@ CREATE TABLE IF NOT EXISTS `sapp`.`evaluations` (
   CONSTRAINT `fk_evaluations_services1`
     FOREIGN KEY (`service_id`)
     REFERENCES `sapp`.`services` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-CREATE TABLE IF NOT EXISTS `sapp`.`addresses` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `city_id` INT NOT NULL,
-  `house_number` INT NULL,
-  `street` VARCHAR(45) NULL,
-  `postal_code` VARCHAR(45) NULL,
-  `district` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_addresses_cities1`
-    FOREIGN KEY (`city_id`)
-    REFERENCES `sapp`.`cities` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_addresses_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `sapp`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
